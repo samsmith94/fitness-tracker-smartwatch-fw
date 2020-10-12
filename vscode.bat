e5bc42bdefb0db9898b18ad6c256478048e461ae
Sámuel Kovács <sunikovacs@gmail.com>
2020-10-12 13:50:38 +0200

    add vscode.bat

---
+++ vscode.bat
@@ -0,0 +1,24 @@
+rem Location of Nordic SDK
+set NRF_SDK=C:/Users/sunik/Desktop/fitness-tracker/smartwatch-fw/nRF5_SDK_17.0.0_9d13099
+
+rem Location of Nordic Command Line tools (nrfjprog) 
+set NRF_TOOLS=C:/Program Files (x86)/Nordic Semiconductor/nrf-command-line-tools/bin
+
+rem location of GCC Cross-compiler https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads
+set GNU_GCC=C:/Program Files (x86)/GNU Tools ARM Embedded/9 2019-q4-major/bin
+
+rem Location of Gnu Tools (make) https://github.com/gnu-mcu-eclipse/windows-build-tools/releases
+set GNU_TOOLS=C:/Users/sunik/AppData/Roaming/GNU MCU Eclipse/Build Tools/2.12-20190422-1053/bin
+
+rem Location of SEGGER JLink tools
+set SEGGER_TOOLS=C:/Program Files (x86)/SEGGER/JLink
+
+rem Location of java
+set JAVA=C:/Program Files/Android/Android Studio/jre/bin/java.exe
+
+
+rem Serial numbers of nRF development boards
+set PCA10056_SN=683798348
+set PCA10040_SN=682645815
+
+"C:/Users/sunik/AppData/Local/Programs/Microsoft VS Code/Code.exe" fitness-tracker.code-workspace
