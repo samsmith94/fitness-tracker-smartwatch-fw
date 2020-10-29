65a2add0294a9f01371d05327861fdc261ac5651
Sámuel Kovács <sunikovacs@gmail.com>
2020-10-29 22:28:49 +0100

    add clock widget

--- src/main.c
+++ src/main.c
@@ -277,7 +277,7 @@ void init_display_pwm(void)
     APP_ERROR_CHECK(err_code);
     app_pwm_enable(&PWM1);
 
-    APP_ERROR_CHECK(app_pwm_channel_duty_set(&PWM1, 1, 5));
+    APP_ERROR_CHECK(app_pwm_channel_duty_set(&PWM1, 1, 1));
 
     /*
     for (int i = 0; i < 5000; i++) {
@@ -366,7 +366,9 @@ void render_stopper_screen(void)
     //ST7735_write_string(80-25, 46, "STOPPER", Font_7x10, ST7735_WHITE, ST7735_BLACK);
 
     st7735_fill_screen(ST7735_BLACK);
-    draw_widget(clock_widget, 10, 40 - (14 / 2));
+    //draw_widget(clock_widget, 10, 40 - (14 / 2));
+    draw_widget(clk_widget, 10, 40 - (14 / 2));
+    
 
     sprintf(stopper_buff, "%02d:%02d.", 0, 0, 0);
     ST7735_write_string(80 - 25, 26, stopper_buff, Font_11x18, ST7735_WHITE, ST7735_BLACK);
@@ -386,7 +388,7 @@ void render_timer_screen(void)
     //ST7735_write_string(80-25, 46, "TIMER", Font_7x10, ST7735_WHITE, ST7735_BLACK);
 
     st7735_fill_screen(ST7735_BLACK);
-    draw_widget(clock_widget, 10, 40 - (14 / 2));
+    draw_widget(tim_widget, 10, 40 - (14 / 2));
 
     sprintf(stopper_buff, "%02d", 10);
     ST7735_write_string(80 - 25, 26, stopper_buff, Font_11x18, ST7735_WHITE, ST7735_BLACK);
@@ -637,6 +639,11 @@ int main(void)
     st7735_init();
     nrf_delay_ms(20);
     st7735_fill_screen(ST7735_BLACK);
+
+    //st7735_invert_colors(false);
+    //st7735_draw_image(0, 0, 40, 40, test_img_40x40);
+    //nrf_delay_ms(1000);
+
 
     //draw_widget(heart_widget, 10, 40-(14/2)-25);
     //draw_widget(battery_widget, 140, 5);
